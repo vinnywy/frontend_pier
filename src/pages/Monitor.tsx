a4e7fb168d6d8102d0e5cc7862fa494afc9ec5ba
vinnywy <viniciusrangel785@gmail.com>
2026-06-24 23:45:22 -0300

    feat: mais alterações

--- src/pages/Monitor.tsx
+++ src/pages/Monitor.tsx
@@ -1,19 +1,10 @@
-import { Camera } from 'lucide-react'
-import { PlaceholderPage } from '../components/layout/PlaceholderPage'
 import DroneStream from "../components/DroneStream";
-
-
-<DroneStream
-  gatewayId="7"
-  apiBaseUrl={import.meta.env.VITE_API_BASE_URL}
-/>
 
 export default function Monitor() {
   return (
-    <PlaceholderPage
-      title="Live Monitor"
-      icon={Camera}
-      description="Acompanhamento do voo ativo em tempo real. Esta tela será integrada ao endpoint /dashboard/voo-ativo/{drone_id}."
+    <DroneStream
+      gatewayId="7"
+      apiBaseUrl={import.meta.env.VITE_API_BASE_URL}
     />
   )
 }
